7a5eaa2d9d5cec0f09473691c5520f61040b41f6
ridhoalrafi1009 <rihdoalrafi@gmail.com>
2023-10-02 16:44:43 +0700

    udah menggunakan tailwindcss

---
+++ gen-23.08-tailwind-css/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter'],
+        oswald: ['Oswald'],
+        sosial: ['Social-Gothic']
+      },
+    },
+  },
+  plugins: [],
+}
+
