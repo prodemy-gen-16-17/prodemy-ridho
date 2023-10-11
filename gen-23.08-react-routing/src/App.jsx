8d3ac3b4aa9a37d1a8f0911f4a8d94277a18fb89
ridhoalrafi1009 <rihdoalrafi@gmail.com>
2023-10-11 22:03:44 +0700

    duplicate from folder useeffect

--- gen-23.08-react-routing/src/App.jsx
+++ gen-23.08-react-routing/src/App.jsx
@@ -1,1 +1,27 @@
+import React from 'react';
+import Home from './pages/home';
+import Header from './components/Header';
+import Headline from './components/Headline';
+import Product from './components/Product';
+import Footer from './components/Footer';
+import { formFooter, linkFooter } from './components/DataFooter';
 
+function App() {
+  return (
+    <>
+      <Header titleHeader="Selamat Berbelanja" />
+      <Home />
+      <Headline
+        feature1="Håndlavet i Danmark"
+        feature2="Naturlige ingredienser"
+        feature3="Produceret på solenergi"
+      />
+      <hr className="my-16" />
+      <Product />
+      <div className="mb-96"></div>
+      <Footer formFooter={formFooter} linkFooter={linkFooter} />
+    </>
+  );
+}
+
+export default App;
